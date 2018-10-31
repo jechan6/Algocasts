252bac9e352b58729c84bd53a6118378dc16f985
Jeffrey Chan <chanmjeffrey@gmail.com>
2018-10-31 15:56:06 -0700

    Add third solution

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -5,18 +5,22 @@
 //   reverse('apple') === 'leppa'
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
-
 function reverse(str) {
-    str = str.split('');
-    let left = 0;
-    let right = str.length-1;
-    while(left < right) {
-        [str[left], str[right]] = [str[right], str[left]];
-        left++;
-        right--;
-    }
-    return str.join('');
+    return str.split('').reduce( (str, char) => {
+        return char + str;
+    }, '');
 }
+// function reverse(str) {
+//     str = str.split('');
+//     let left = 0;
+//     let right = str.length-1;
+//     while(left < right) {
+//         [str[left], str[right]] = [str[right], str[left]];
+//         left++;
+//         right--;
+//     }
+//     return str.join('');
+// }
 
 // function reverse(str) {
 //     return str.split('').reverse().join('');    
