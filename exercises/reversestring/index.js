f78a13a875ead171a0269e1310aa55569cdc707f
Jeffrey Chan <jchan40@ucsc.edu>
2018-10-31 15:53:07 -0700

    Reverse string

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,20 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+    str = str.split('');
+    let left = 0;
+    let right = str.length-1;
+    while(left < right) {
+        [str[left], str[right]] = [str[right], str[left]];
+        left++;
+        right--;
+    }
+    return str.join('');
+}
+
+// function reverse(str) {
+//     return str.split('').reverse().join('');    
+// }
 
 module.exports = reverse;
