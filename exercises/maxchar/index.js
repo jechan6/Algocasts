045be734ddf9078eb380ee41756bcf08ca2b743e
Jeffrey Chan <chanmjeffrey@gmail.com>
2018-10-31 17:03:22 -0700

    Max char

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,22 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    const chars = {};
+    for(let el of str) {
+        chars[el] = chars[el] + 1 || 1; 
+    }
+        
+   
+    let maxChar = '';
+    let maxCount = 0;
+    Object.keys(chars).forEach( key => {
+        if(chars[key] > maxCount) {
+            maxChar = key;
+            maxCount = chars[key];
+        }
+    })
+    return maxChar;
+}
 
 module.exports = maxChar;
