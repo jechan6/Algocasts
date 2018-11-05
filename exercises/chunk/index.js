6bd08b08b7d8bfd2d1f76ebcdbcaaef17ca409cd
Jeffrey Chan <chanmjeffrey@gmail.com>
2018-11-04 16:53:16 -0800

    Chunk array

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,18 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+    let result = [];
+    let subArr = [];
+    for(let i = 0; i<array.length; i++) {
+        subArr.push(array[i]);
+        if(subArr.length === size) {
+            result.push(subArr);
+            subArr = [];
+        }
+    }
+    if(subArr.length > 0) result.push(subArr);
+    return result;
+}
 
 module.exports = chunk;
